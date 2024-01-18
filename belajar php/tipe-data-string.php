1975c27c0fc9c7bfa5d09c8165ccc2e513702138
bella <111962970+khairunisabellaaa@users.noreply.github.com>
2024-01-18 08:36:07 +0700

    Add files via upload

---
+++ belajar php/tipe-data-string.php
@@ -0,0 +1,12 @@
+<?php
+
+echo "ini tipe data sting \n";
+var_dump("hallo ");
+
+echo <<<HELLO
+ini adalah teks panjang
+jaaadiiii
+iinnnii
+adaaalaah
+heredoc
+HELLO;
---
+++ belajar php/variable.php
@@ -0,0 +1,15 @@
+<?php
+
+$nama = "Bella";
+$umur = 16;
+
+echo "Nama : ";
+echo $nama;
+echo "\n";
+echo "umur : ";
+echo $umur;
+echo "\n";
+
+$kelas = "11 rpl 1";
+
+echo"kelas : " . $kelas;
